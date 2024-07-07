6e2584df6947c6086757af3d6c16729e8baff2f8
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-07-07 17:05:40 -0500

    Conditional Button Color

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,14 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
   styles: ``
 })
-export class IncrementadorComponent {
+export class IncrementadorComponent implements OnInit {
+
+  ngOnInit () {
+    this.btnClass = `btn ${ this.btnClass }`;
+  };
 
   // @Input() progreso: number = 50;
   @Input( 'valor' ) progreso: number = 50;
+  @Input() btnClass: string = 'btn-primary';
 
   @Output( 'valor' ) valorSalida: EventEmitter<number> = new EventEmitter();
 
