0b5295bed7665beb87044e0c34936599ba3275c5
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-07-08 02:49:19 -0500

    Adding CSS classes without using ngClass

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-account-settings',
   templateUrl: './account-settings.component.html',
   styles: ``
 })
-export class AccountSettingsComponent {
+export class AccountSettingsComponent implements OnInit {
 
   public linkTheme = document.querySelector('#theme');
+  public links!: NodeListOf<Element>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
+  };
 
   changeTheme( theme: string ) {
     
@@ -15,5 +23,21 @@ export class AccountSettingsComponent {
     
     this.linkTheme?.setAttribute('href', url);
     localStorage.setItem('theme', url);
+
+    this.checkCurrentTheme();
+  };
+
+  checkCurrentTheme() {
+
+    this.links.forEach( elem => {
+      elem.classList.remove('working');
+      const btnTheme = elem.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${ btnTheme }.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href');
+
+      if ( btnThemeUrl === currentTheme ) {
+        elem.classList.add('working');
+      };
+    });
   };
 }
