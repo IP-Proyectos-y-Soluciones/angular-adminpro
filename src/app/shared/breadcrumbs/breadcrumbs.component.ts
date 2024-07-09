d8a01352e564dd0970cee6eaaf124c6173fc8687
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-07-09 03:47:29 -0500

    Breadcrumbs using observables and route parameters

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styles: ``
+  styles: ``,
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
 
+  public title: string = '';
+
+  constructor( private router: Router ) {
+
+    this.getArgRuta();
+  }
+
+  ngOnDestroy(): void {}
+
+  getArgRuta() {
+    this.router.events
+    .pipe(
+      filter( event => event instanceof ActivationEnd ),
+      map( event => event as ActivationEnd ),
+      filter( ( event: ActivationEnd ) => event.snapshot.firstChild === null ),
+      map( ( event: ActivationEnd ) => event.snapshot.data ),
+    )
+    .subscribe( ({ title }) => {
+      // console.log( title );
+      this.title = title;
+      document.title = `AdminPro - ${ title }`;
+    });
+  }
 }
