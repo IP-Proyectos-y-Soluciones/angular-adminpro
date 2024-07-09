e6da3749ddb5eeab17c618b9413e64db8bb6a7ef
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-07-09 04:05:54 -0500

    Breadcrumb optimizations

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 @Component({
@@ -8,28 +9,33 @@ import { filter, map } from 'rxjs/operators';
   styles: ``,
 })
 export class BreadcrumbsComponent implements OnDestroy {
-
+  
   public title: string = '';
+  public titleSubs$: Subscription;
 
   constructor( private router: Router ) {
 
-    this.getArgRuta();
-  }
-
-  ngOnDestroy(): void {}
-
-  getArgRuta() {
-    this.router.events
-    .pipe(
-      filter( event => event instanceof ActivationEnd ),
-      map( event => event as ActivationEnd ),
-      filter( ( event: ActivationEnd ) => event.snapshot.firstChild === null ),
-      map( ( event: ActivationEnd ) => event.snapshot.data ),
-    )
-    .subscribe( ({ title }) => {
+    this.titleSubs$ = this.getArgRuta().subscribe(({ title }) => {
       // console.log( title );
       this.title = title;
       document.title = `AdminPro - ${ title }`;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.titleSubs$.unsubscribe();
+  }
+
+  /**
+   * @ame getArgRuta
+   * @description Modulo para definir las rutas de manera dinámica utilizando Observable y parametros
+   */
+  getArgRuta() {
+    return this.router.events.pipe(
+      filter(( event ) => event instanceof ActivationEnd ),
+      map(( event ) => event as ActivationEnd),
+      filter(( event: ActivationEnd ) => event.snapshot.firstChild === null ),
+      map(( event: ActivationEnd ) => event.snapshot.data )
+    );
   }
 }
