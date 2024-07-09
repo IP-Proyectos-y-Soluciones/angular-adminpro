0ba57d9ff4c7cd3fc995a5c5b984d065ba2f88f8
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-07-08 23:25:01 -0500

    Rxjs component and fix in menu

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -15,6 +15,7 @@ import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 
 
@@ -26,6 +27,7 @@ import { PromesasComponent } from './promesas/promesas.component';
     PagesComponent,
     AccountSettingsComponent,
     PromesasComponent,
+    RxjsComponent,
   ],
   exports: [
     DashboardComponent,
@@ -33,7 +35,6 @@ import { PromesasComponent } from './promesas/promesas.component';
     Grafica1Component,
     PagesComponent,
     AccountSettingsComponent,
-    PromesasComponent,
   ],
   imports: [
     CommonModule,
