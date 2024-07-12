cad1de1c903eaf4937de26582d923ea8de718ebf
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-07-12 07:30:42 -0500

    Validate equal passwords

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,11 +12,11 @@ export class RegisterComponent {
 
   public registerForm = this.fb.group({
     name: [
-      'User Test',
+      '',
       [ Validators.required, Validators.minLength( 6 ), Validators.maxLength( 30 ) ],
     ],
     email: [ 
-      'test@correo.com', 
+      '', 
       [ Validators.required, Validators.email ], 
     ],
     password: [ 
@@ -28,6 +28,8 @@ export class RegisterComponent {
       [ Validators.required, Validators.minLength( 6 ), Validators.maxLength( 16 ) ], 
     ],
     terms: [ false, Validators.required ],
+  }, {
+    Validators: this.passwordsIguales( 'password', 'password2' ),
   });
 
   constructor( private fb: FormBuilder ) {}
@@ -64,6 +66,17 @@ export class RegisterComponent {
   };
 
   /**
+   * @name contrasenasNoValidas
+   * @description Esta función verifica si las contraseñas ingresadas no son iguales.
+   * @returns { boolean } - Retorna `true` si las contraseñas no coinciden, de lo contrario retorna `false`.
+   */
+  contrasenasNoValidas(): boolean {
+    const pass1 = this.registerForm.get('password')?.value;
+    const pass2 = this.registerForm.get('password2')?.value;
+    return ( pass1 !== pass2 ) && this.formSubmitted;
+  };
+
+  /**
    * @name aceptaTerminos
    * @description Esta función verifica si los términos fueron aceptados.
    * @returns { boolean } - Retorna `true` si los términos no fueron aceptados, de lo contrario retorna `false`.
@@ -71,5 +84,40 @@ export class RegisterComponent {
   aceptaTerminos(): boolean {
     const terms = this.registerForm.get( 'terms' )?.value;
     return !terms && this.formSubmitted;
+  };
+
+  /**
+   * @name passwordsIguales
+   * @description Esta función es un validador personalizado para Angular Reactive Forms. 
+   * Compara dos campos de contraseña dentro de un FormGroup y establece un error en el segundo campo si las contraseñas no coinciden.
+   * Este validador se aplica a un FormGroup completo, no a un control individual.
+   * @param pass1Name { string } - El nombre del primer campo de contraseña en el formulario.
+   * @param pass2Name { string } - El nombre del segundo campo de contraseña en el formulario.
+   * @returns { Function } - Retorna una función que toma un FormGroup como argumento. Esta función compara los valores de los dos campos de contraseña:
+   *  - Si las contraseñas coinciden, elimina cualquier error en el segundo campo de contraseña.
+   *  - Si las contraseñas no coinciden, establece un error `{ noEsIgual: true }` en el segundo campo de contraseña.
+   * @example 
+   * Uso en un formulario reactivo
+   * this.form = this.fb.group({
+   *   password: ['', Validators.required],
+   *   confirmPassword: ['', Validators.required],
+   * }, {
+   *   validators: this.passwordsIguales('password', 'confirmPassword')
+   * });
+   */
+  passwordsIguales ( pass1Name: string, pass2Name: string ) {
+    
+    return ( formGroup: FormGroup ) => {
+      const pass1Control = formGroup.get( pass1Name );
+      const pass2Control = formGroup.get( pass2Name );
+
+      if ( pass1Control && pass2Control ) {
+        if ( pass1Control.value === pass2Control.value ) {
+          pass2Control.setErrors( null );
+        } else {
+          pass2Control.setErrors({ noEsIgual: true });
+        };
+      };
+    };
   };
 }
