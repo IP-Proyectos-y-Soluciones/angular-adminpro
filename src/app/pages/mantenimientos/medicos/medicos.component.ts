46b3c9252943643df886ee8302d20178c7f1ee4c
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-07-25 05:26:09 -0500

    Medical Services - CRUD

--- src/app/pages/mantenimientos/medicos/medicos.component.ts
+++ src/app/pages/mantenimientos/medicos/medicos.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+
+import { Medico } from '../../../models/medico.model';
+
+import { MedicoService } from '../../../services/medico.service';
+import { ModalImagenService } from '../../../services/modal-imagen.service';
+
 
 @Component({
   selector: 'app-medicos',
   templateUrl: './medicos.component.html',
   styles: ``
 })
-export class MedicosComponent {
+export class MedicosComponent implements OnInit {
 
+  public cargando: boolean = true;
+  public medicos: Medico[] = [];
+
+  /**
+   * @constructor
+   * @description Constructor del componente que se utiliza para inyectar los servicios necesarios en la clase del componente. 
+   * El constructor recibe dos servicios: `MedicoService` para la gestión de datos de médicos y `ModalImagenService` 
+   * para la manipulación de modales de imágenes. Estos servicios se inyectan a través de la dependencia del constructor para ser utilizados 
+   * en los métodos del componente.
+   * @param { MedicoService } medicoService - Servicio encargado de las operaciones relacionadas con los médicos, como obtener, crear, actualizar y eliminar médicos.
+   * @param { ModalImagenService } modalImagenService - Servicio encargado de la gestión de imágenes en los modales, permitiendo abrir y manipular modales de imagen.
+   */
+  constructor(
+    private medicoService: MedicoService, 
+    private modalImagenService: ModalImagenService, 
+  ) { }
+
+  /**
+   * @name ngOnInit
+   * @description Este método es parte del ciclo de vida del componente en Angular. Se ejecuta una vez que Angular ha inicializado todas las propiedades de entrada del componente. 
+   * En este caso, se utiliza para cargar la lista de médicos llamando al método `cargarMedicos`.
+   * @returns { void } - Este método no devuelve ningún valor.
+   */
+  ngOnInit (): void {
+    this.cargarMedicos();
+  };
+
+  /**
+   * @name cargarMedicos
+   * @description Este método se encarga de cargar la lista de médicos desde el servicio `MedicoService`. 
+   * Primero, establece la propiedad `cargando` en `true` para indicar que se está realizando una carga de datos. 
+   * Luego, llama al método `cargarMedicos` del servicio `MedicoService`, que realiza una solicitud HTTP para obtener la lista de médicos. 
+   * Una vez que se recibe la respuesta, se actualiza la propiedad `medicos` con los datos recibidos y se establece `cargando` en `false` para indicar que la carga ha finalizado.
+   * @returns { void } - Este método no devuelve ningún valor.
+   */
+  cargarMedicos(): void {
+    this.cargando = true;
+    this.medicoService.cargarMedicos()
+     .subscribe( medicos => {
+        this.cargando = false;
+        this.medicos = medicos;
+      });
+  };
+
+  /**
+   * @name abrirModal
+   * @description Este método se encarga de abrir un modal para gestionar las imágenes asociadas a un médico. 
+   * Primero, verifica si el médico tiene un ID definido. Si el ID está presente, llama al método `abrirModal` del servicio `ModalImagenService` 
+   * pasando el tipo de entidad ('medicos'), el ID del médico y la imagen asociada. Si el ID no está definido, muestra una alerta de error 
+   * utilizando `Swal.fire` indicando que el médico no tiene un ID definido.
+   * @param { Medico } medico - El objeto `Medico` cuyos datos de imagen serán gestionados en el modal.
+   * @returns { void } - Este método no devuelve ningún valor.
+   */
+  abrirModal( medico: Medico ): void {
+
+    if ( medico._id ) {
+      this.modalImagenService.abrirModal( 'medicos', medico._id, medico.img );
+    } else {
+      Swal.fire( 'Error', 'El medico no tiene ID definido', 'error' );
+    };
+  };
 }
--- src/app/services/medico.service.ts
+++ src/app/services/medico.service.ts
@@ -1,9 +1,102 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+import { Medico } from '../models/medico.model';
+
+const base_url = environment.base_url;
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicoService {
 
-  constructor() { }
+  /**
+   * @constructor
+   * @description Constructor del componente. Este método se llama al crear una nueva instancia de la clase. 
+   * Se inyecta el servicio HttpClient que permite realizar solicitudes HTTP desde la aplicación.
+   * @param { HttpClient } http - Servicio de Angular para realizar solicitudes HTTP.
+   */
+  constructor(
+    private http: HttpClient, 
+  ) { }
+
+  /**
+   * @name token
+   * @description Este método obtiene el token almacenado en el localStorage del navegador. 
+   * Si no se encuentra ningún token, devuelve una cadena vacía.
+   * @returns { string } - El token de autenticación almacenado en el localStorage, o una cadena vacía si no se encuentra ningún token.
+   */
+  get token(): string {
+    return localStorage.getItem( 'token' ) || '';
+  };
+
+  /**
+   * @name headers
+   * @description Este método devuelve un objeto de encabezados HTTP que incluyen un token de autenticación.
+   * @returns { HttpHeaders } - Un objeto de encabezados HTTP que incluye un token de autenticación.
+   */
+  get headers(): HttpHeaders {
+    return new HttpHeaders({
+      'x-token': this.token
+    });
+  };
+
+  /**
+   * @name cargarMedicos
+   * @description Este método realiza una solicitud HTTP GET al servidor para obtener una lista de médicos. 
+   * La URL de la solicitud se construye utilizando una variable base de la URL (`base_url`). 
+   * La solicitud incluye encabezados de autenticación. 
+   * La respuesta del servidor, que contiene un objeto con una propiedad `medicos`, 
+   * se transforma mediante un operador `map` para devolver solo la lista de médicos.
+   * @returns { Observable<Medico[]> } - Un observable que emite un arreglo de objetos `Medico`.
+   */
+  cargarMedicos(): Observable<Medico[]> {
+    const url = `${ base_url }/medicos`;
+    return this.http.get<{ ok: boolean, medicos: Medico[] }>( url, { headers: this.headers } )
+      .pipe(
+        map( resp  => resp.medicos )
+      );
+  };
+
+  /**
+   * @name crearMedico
+   * @description Este método realiza una solicitud HTTP POST al servidor para crear un nuevo médico. 
+   * La URL de la solicitud se construye utilizando una variable base de la URL (`base_url`). 
+   * La solicitud incluye encabezados de autenticación y envía los datos del médico en el cuerpo de la solicitud.
+   * @param { Medico } medico - El objeto `Medico` que contiene los datos del nuevo médico que se va a crear.
+   * @returns { Observable<Object> } - Un observable que emite la respuesta del servidor, la cual contiene la información del médico creado.
+   */
+  crearMedico( medico: Medico ): Observable<Object> {
+    const url = `${ base_url }/medicos`;
+    return this.http.post( url, medico, { headers: this.headers } );
+  };
+
+  /**
+   * @name actualizarMedico
+   * @description Este método realiza una solicitud HTTP PUT al servidor para actualizar la información de un médico existente. 
+   * La URL de la solicitud se construye utilizando una variable base de la URL (`base_url`) y el ID del médico. 
+   * La solicitud incluye encabezados de autenticación y envía los datos actualizados del médico en el cuerpo de la solicitud.
+   * @param { Medico } medico - El objeto `Medico` que contiene los datos actualizados del médico que se va a actualizar.
+   * @returns { Observable<Object> } - Un observable que emite la respuesta del servidor, la cual contiene la información del médico actualizado.
+   */
+  actualizarMedico( medico: Medico ): Observable<Object> {
+    const url = `${ base_url }/medicos/${ medico._id }`;
+    return this.http.put( url, medico, { headers: this.headers } );
+  };
+
+  /**
+   * @name borrarMedico
+   * @description Este método realiza una solicitud HTTP DELETE al servidor para eliminar un médico existente. 
+   * La URL de la solicitud se construye utilizando una variable base de la URL (`base_url`) y el ID del médico. 
+   * La solicitud incluye encabezados de autenticación para asegurar que solo usuarios autorizados puedan realizar la eliminación.
+   * @param { string } _id - El ID del médico que se va a eliminar.
+   * @returns { Observable<Object> } - Un observable que emite la respuesta del servidor tras la eliminación del médico.
+   */
+  borrarMedico( _id: string ): Observable<Object> {
+    const url = `${ base_url }/medicos/${ _id }`;
+    return this.http.delete( url, { headers: this.headers } );
+  };
 }
